b2bc90f6d3f5aa404515e35db15a553b7b545cc0
tejesh <tejeshreddy250@gmail.com>
2024-11-29 17:11:49 -0600

    modified-needblood

--- lib/needblood.dart
+++ lib/needblood.dart
@@ -1,7 +1,8 @@
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:country_state_city_picker/country_state_city_picker.dart';
 import 'package:flutter/material.dart';
-// import 'package:country_state_city_picker/country_state_city_picker.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Needblood extends StatefulWidget {
   const Needblood({super.key});
@@ -11,30 +12,29 @@ class Needblood extends StatefulWidget {
 }
 
 class _NeedbloodState extends State<Needblood> {
+  final TextEditingController _controller = TextEditingController();
+  List<QueryDocumentSnapshot<Map<String, dynamic>>> docs = [];
+  final List<String> items = ["A+", "A−", 'B+', 'B−', 'AB+', 'AB−', 'O+', 'O−'];
+  late String countryValue = "";
+  late String stateValue = "";
+  late String cityValue = "";
+  bool isLoading = true;
+
   @override
   void initState() {
-    fetchdata();
     super.initState();
+    fetchdata();
   }
 
-  final TextEditingController _controller = TextEditingController();
-  var items = [
-    "A+",
-    "A−",
-    'B+',
-    'B−',
-    'AB+',
-    'AB−',
-    'O+',
-    'O−',
-  ];
-  int count = 0;
-  bool check = true;
-  late String countryValue;
-  late String stateValue;
-  late String cityValue;
+  Future<void> fetchdata() async {
+    QuerySnapshot<Map<String, dynamic>> data =
+        await FirebaseFirestore.instance.collection("users").get();
+    docs = data.docs;
+    setState(() {
+      isLoading = false;
+    });
+  }
 
-  List<QueryDocumentSnapshot<Map<String, dynamic>>> docs = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,241 +45,22 @@ class _NeedbloodState extends State<Needblood> {
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.fromLTRB(8.0, 20.0, 8, 8),
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Padding(
-                padding: EdgeInsets.fromLTRB(15.0, 2.0, 0.0, 0.0),
-                child: Text(
-                  'Enter the following details to get the profiles',
-                  style: TextStyle(fontSize: 20, color: Colors.red),
-                  textAlign: TextAlign.left,
-                ),
+              const Text(
+                'Enter the following details to get the profiles',
+                style: TextStyle(fontSize: 20, color: Colors.red),
               ),
-              Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  // height: 600,
-                  child: Column(
-                    children: [
-                      SelectState(
-                        onCountryChanged: (value) {
-                          setState(() {
-                            countryValue = value;
-                          });
-                        },
-                        onStateChanged: (value) {
-                          setState(() {
-                            stateValue = value;
-                          });
-                        },
-                        onCityChanged: (value) {
-                          setState(() {
-                            cityValue = value;
-                          });
-                        },
-                      ),
-                      // InkWell(
-                      //   onTap:(){
-                      //     print('country selected is $countryValue');
-                      //     print('country selected is $stateValue');
-                      //     print('country selected is $cityValue');
-                      //   },
-                      //   child: Text(' Check')
-                      // )
-                    ],
-                  )),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  controller: _controller,
-                  enabled: true,
-                  decoration: InputDecoration(
-                    hintText: 'Select the blood group you need',
-                    hintStyle: const TextStyle(color: Colors.red),
-                    fillColor: Colors.green[200],
-                    suffixIcon: PopupMenuButton<String>(
-                      // color: Colors.red,
-
-                      icon: const Icon(
-                        Icons.arrow_drop_down,
-                        color: Colors.black,
-                      ),
-                      onSelected: (String value) {
-                        _controller.text = value;
-                        setState(() {});
-                      },
-                      itemBuilder: (BuildContext context) {
-                        return items.map<PopupMenuItem<String>>((String value) {
-                          return PopupMenuItem(
-                              value: value,
-                              child: Text(value));
-                        }).toList();
-                      },
-                    ),
-                  ),
-                ),
-              ),
-              if (docs.isNotEmpty)
-                ListView.builder(
-                  physics: const NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  itemCount: docs.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    // print(docs[index].data()['blood']);
-                    // print(_controller.text);
-
-                    if (docs[index].data()['blood'] ==
-                            _controller.text.toUpperCase() &&
-                        docs.isNotEmpty &&
-                        cityValue == docs[index].data()['city']) {
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          height: 220,
-                          width: 1000,
-                          decoration: BoxDecoration(
-                              color: Colors.green[300],
-                              borderRadius:
-                                  const BorderRadius.all(Radius.circular(20))),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              const Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Padding(
-                                    padding: EdgeInsets.all(8.0),
-                                    child: Text(
-                                      'Name:',
-                                      style: TextStyle(
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: EdgeInsets.all(8.0),
-                                    child: Text(
-                                      'Email:',
-                                      style: TextStyle(
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Text(
-                                      docs[index].data()['name'],
-                                      style: const TextStyle(
-                                          color: Colors.white, fontSize: 15),
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Text(
-                                      docs[index].data()['email'],
-                                      style: const TextStyle(
-                                          color: Colors.white, fontSize: 15),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              const Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Padding(
-                                    padding: EdgeInsets.all(8.0),
-                                    child: Text(
-                                      'Phone:',
-                                      style: TextStyle(
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: EdgeInsets.all(8.0),
-                                    child: Text(
-                                      'Blood Group:',
-                                      style: TextStyle(
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Text(
-                                      docs[index].data()['phone'],
-                                      style: const TextStyle(
-                                          color: Colors.white, fontSize: 15),
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Text(
-                                      docs[index].data()['blood'],
-                                      style: const TextStyle(
-                                          color: Colors.white, fontSize: 15),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              const Row(
-                                children: [
-                                  Padding(
-                                    padding: EdgeInsets.all(8.0),
-                                    child: Text(
-                                      'City:',
-                                      style: TextStyle(
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Text(
-                                      docs[index].data()['city'],
-                                      style: const TextStyle(
-                                          color: Colors.white, fontSize: 15),
-                                    ),
-                                  ),
-                                ],
-                              )
-                            ],
-                          ),
-                        ),
-                      );
-                    }
-
-                    return index == 0 && _controller.text.isNotEmpty
-                        ? Container(
-                            child: const Text(
-                              'Donors are not available in your city.Kindly check the registered users in home page.',
-                              style: TextStyle(color: Colors.red),
-                            ),
-                          )
-                        : Container();
-                  },
-                )
-              else
-                const CircularProgressIndicator()
+              const SizedBox(height: 20),
+              _buildLocationPicker(),
+              const SizedBox(height: 20),
+              _buildBloodGroupSelector(),
+              const SizedBox(height: 20),
+              isLoading
+                  ? const Center(child: CircularProgressIndicator())
+                  : _buildDonorList(),
             ],
           ),
         ),
@@ -287,12 +68,133 @@ class _NeedbloodState extends State<Needblood> {
     );
   }
 
-  Future<List> fetchdata() async {
-    QuerySnapshot<Map<String, dynamic>> data =
-        await FirebaseFirestore.instance.collection("users").get();
-    docs = data.docs;
-    // print(docs);
-    setState(() {});
-    return docs;
+  Widget _buildLocationPicker() {
+    return Card(
+      elevation: 3,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SelectState(
+          onCountryChanged: (value) {
+            setState(() => countryValue = value);
+          },
+          onStateChanged: (value) {
+            setState(() => stateValue = value);
+          },
+          onCityChanged: (value) {
+            setState(() => cityValue = value);
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBloodGroupSelector() {
+    return Card(
+      elevation: 3,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: TextField(
+          controller: _controller,
+          decoration: InputDecoration(
+            labelText: 'Select the blood group you need',
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+            suffixIcon: PopupMenuButton<String>(
+              icon: const Icon(Icons.arrow_drop_down),
+              onSelected: (value) {
+                _controller.text = value;
+                setState(() {});
+              },
+              itemBuilder: (context) {
+                return items.map((String value) {
+                  return PopupMenuItem(value: value, child: Text(value));
+                }).toList();
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDonorList() {
+    final filteredDocs = docs.where((doc) {
+      return doc['blood'] == _controller.text.toUpperCase() &&
+          cityValue == doc['city'];
+    }).toList();
+
+    if (_controller.text.isEmpty || cityValue.isEmpty) {
+      return const Center(
+        child: Text(
+          'Please select all details to see the donor profiles.',
+          style: TextStyle(color: Colors.grey, fontSize: 16),
+        ),
+      );
+    }
+
+    if (filteredDocs.isEmpty) {
+      return const Center(
+        child: Text(
+          'No donors available in your city. Kindly check the registered users on the home page.',
+          style: TextStyle(color: Colors.red, fontSize: 16),
+          textAlign: TextAlign.center,
+        ),
+      );
+    }
+
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: filteredDocs.length,
+      itemBuilder: (context, index) {
+        final doc = filteredDocs[index];
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 16.0),
+          child: Card(
+            elevation: 3,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.red,
+                child: Text(
+                  doc['blood'],
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ),
+              title: Text('Name: ${doc['name']}'),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Email: ${doc['email']}'),
+                  Text('Phone: ${doc['phone']}'),
+                  Text('City: ${doc['city']}'),
+                ],
+              ),
+              trailing: IconButton(
+                icon: const Icon(Icons.call, color: Colors.green),
+                onPressed: () async {
+                  // final phoneNumber =
+                  //     doc['phone']; // Assuming phone number is stored here
+
+                  // // Construct the URL for dialing
+                  // final uri = Uri(scheme: 'tel', path: phoneNumber);
+
+                  // // Launch the URL to open the dialer
+                  // if (await canLaunchUrl(uri)) {
+                  //   await launchUrl(uri);
+                  // } else {
+                  //   // Handle the error if the URL can't be launched
+                  //   print('Could not launch $uri');
+                  // }
+                },
+              ),
+            ),
+          ),
+        );
+      },
+    );
   }
 }
